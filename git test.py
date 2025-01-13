8761fb2a0d92345fb5418127c2963bf6f0bf8c71
Mostafa <mobada8484@gmail.com>
2025-01-13 14:24:47 +0330

    اضافه کرئن منها

--- git test.py
+++ git test.py
@@ -17,15 +17,29 @@ print(z,{time.time()})
 z_valid = zarb(123499999999999999999999999999999999, 22222)  # ورودی معتبر
 print(f"نتیجه ضرب: {z_valid},,,,,,,,{time.time()}")
 
-def taghsim(adad_aval,adad_dovom):
+# def taghsim(adad_aval,adad_dovom):
+#     a=adad_aval
+#     b=adad_dovom
+#     if not isinstance(adad_aval, (int, float)) or not isinstance(adad_dovom, (int, float)):
+#         return 'Error: لطفاً عدد صحیح یا اعشاری وارد کنید!'
+#
+#     else:
+#         c = a/b
+#         return c
+#
+# print(taghsim(55555515, 2))
+
+
+def menha(adad_aval,adad_dovom):
     a=adad_aval
     b=adad_dovom
     if not isinstance(adad_aval, (int, float)) or not isinstance(adad_dovom, (int, float)):
         return 'Error: لطفاً عدد صحیح یا اعشاری وارد کنید!'
 
     else:
-        c = a/b
+        c = a-b
         return c
 
-print(taghsim(55555515, 2))
+print(menha(5, 2))
+
 
